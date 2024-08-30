68db0db78bf2f38bc1ace4838905f846f6cac0aa
RMuhammadali07 <muhammadalirivojiddinov07@gmail.com>
2024-08-30 07:41:43 +0500

    portfolio

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,72 @@
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/Design uten navn.png';
+// import heroImg from '../../assets/photo_2024-08-28_09-35-18.jpg';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/cv.pdf';
+import { useTheme } from '../../common/ThemeContext';
+
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          src={heroImg}
+          className={styles.hero}
+          style={{ borderRadius: "50%", height: "400px" }}
+          alt="Profile picture of Harris Johnsen"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Rivojiddinov
+          <br />
+          Muhammadali
+        </h1>
+        <h2>Frontend Developer</h2>
+        <span>
+          <a href="https://x.com/ZIMON_07" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+          <a href="https://github.com/RMuhammadali07" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/muhammadali-rivojiddinov-555708325/"
+            target="_blank"
+          >
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+        </span>
+        <p className={styles.description}>
+          Tijorat korxonalari uchun zamonaviy React veb-ilovalarini ishlab
+          chiqish ishtiyoqi bilan.
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
