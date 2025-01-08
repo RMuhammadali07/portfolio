180f221cf4f844fd6b02753adc7a9ce3ade7d607
RMuhammadali07 <muhammadalirivojiddinov07@gmail.com>
2025-01-08 16:21:14 +0500

    footer new year

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2024 Rivojiddinov Muhammadali. <br />
+        &copy; 2025 Rivojiddinov Muhammadali. <br />
         All rights reserved.
       </p>
     </section>
